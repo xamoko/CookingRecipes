2b6ce305f9524bd8523ad3df9d8229a689ca39fd
Javier Morales <javier.mora.es@vantis.mx>
2023-07-18 00:26:21 -0600

    Initial Commit

---
+++ CookingRecipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookingRecipes
+//
+//  Created by Xavier Morales on 18/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
